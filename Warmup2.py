6596a001ab854cfe1bf7c76e3364fb05e8787e9d
Ahmed Shoulah <shoulah@gmail.com>
2020-01-15 17:56:10 +0200

    string_bits

--- Warmup2.py
+++ Warmup2.py
@@ -5,7 +5,7 @@
 
 print(string_times("ah", 3)) """
 
-def front_times(str, n):
+""" def front_times(str, n):
     if len(str) < 3:
         return n * str
     else:
@@ -13,4 +13,19 @@ def front_times(str, n):
 
 print(front_times("Ahmed", 2)) 
 print(front_times("Shoulah", 3))
-print(front_times("ah", 3))       
+print(front_times("ah", 3))   """     
+
+
+def string_bits(str):
+    if len(str) == 1:
+        return str
+    else:
+        result = ""
+        i = 0
+        while i < len(str):
+            if i % 2 == 0:
+                result += str[i]
+            i += 1
+        return result
+
+print(string_bits("Shoulah"))            
