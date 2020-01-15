b33550b104127007e378129bdd4562c863e08412
Ahmed Shoulah <shoulah@gmail.com>
2020-01-15 18:24:36 +0200

    array_count9

--- Warmup2.py
+++ Warmup2.py
@@ -52,7 +52,7 @@ last2('hixxhi') → 1
 last2('xaxxaxaxx') → 1
 last2('axxxaaxx') → 2 """
 
-
+""" 
 def last2(str):
     counter = 0 #initialize the counter 
     if len(str) > 2:
@@ -66,3 +66,22 @@ def last2(str):
     return counter
 
 print(last2("axxxaaxx"))        
+ """
+
+"""  Given an array of ints, return the number of 9's in the array.
+
+
+array_count9([1, 2, 9]) → 1
+array_count9([1, 9, 9]) → 2
+array_count9([1, 9, 9, 3, 9]) → 3 """
+
+def array_count9(nums):
+    counter = 0
+    for n in nums:
+        if n == 9:
+            counter += 1
+
+    return counter
+
+print(array_count9([1, 9,9]))
+print(array_count9([1]))
