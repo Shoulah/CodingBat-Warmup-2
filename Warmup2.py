3bb9673acf55580e83177ac4a07daeca2748dee9
Ahmed Shoulah <shoulah@gmail.com>
2020-01-15 17:49:32 +0200

    front_times

--- Warmup2.py
+++ Warmup2.py
@@ -1,6 +1,16 @@
-def string_times(str, n):
+""" def string_times(str, n):
     if n >= 0:
         return str * n
 
 
-print(string_times("ah", 3))
+print(string_times("ah", 3)) """
+
+def front_times(str, n):
+    if len(str) < 3:
+        return n * str
+    else:
+        return str[0:3] * n
+
+print(front_times("Ahmed", 2)) 
+print(front_times("Shoulah", 3))
+print(front_times("ah", 3))       
