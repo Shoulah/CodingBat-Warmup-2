2da50f6871b753621daac9737ea14b983c135aaa
Ahmed Shoulah <shoulah@gmail.com>
2020-01-15 18:16:50 +0200

    last2

--- Warmup2.py
+++ Warmup2.py
@@ -30,7 +30,7 @@ def string_bits(str):
 
 print(string_bits("Shoulah"))            
  """
-
+""" 
 def string_splosion(str):
     if len(str) > 0:
         result = ""
@@ -40,4 +40,29 @@ def string_splosion(str):
             i += 1
         return result
 
-print(string_splosion("Code"))        
+print(string_splosion("Code"))     
+ """   
+
+""" 
+Given a string, return the count of the number of times that a substring length 2 appears in 
+the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+
+last2('hixxhi') → 1
+last2('xaxxaxaxx') → 1
+last2('axxxaaxx') → 2 """
+
+
+def last2(str):
+    counter = 0 #initialize the counter 
+    if len(str) > 2:
+        subString = str[len(str) - 2 : len(str)]
+        
+        i = 0
+        while i < len(str) - 2:
+            if str[i:i + 2] == subString:
+                counter +=1
+            i +=1
+    return counter
+
+print(last2("axxxaaxx"))        
